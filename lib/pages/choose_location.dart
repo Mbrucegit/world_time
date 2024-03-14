9138c26ba6965401d2f2329c5558fe3851da4f16
Qiang Fu <tactiturn@hotmail.com>
2024-03-15 02:37:43 +0800

    Added Asynchronous function getData() in Choose lcoation file.

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -1,5 +1,6 @@
 import 'dart:ffi';
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 class ChooseLocation extends StatefulWidget {
@@ -12,11 +13,28 @@ class ChooseLocation extends StatefulWidget {
 class _ChooseLocationState extends State<ChooseLocation> {
 
   int counter = 0;
+  void getData() async {
+    //simulate network request for a user name
+    String username = await Future.delayed(const Duration(seconds: 1),(){
+      print('username function');
+      return 'Yoshi';
+    });
+
+    //simulate network request for the bio of user name
+    await Future.delayed(const Duration(seconds: 2), (){
+        return 'A second has passed for $username';
+    });
+    print('$username');
+  }
+
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
     print('initState function ran;');
+    getData();
+    print('initState function ran2;');
+
   }
 
 
